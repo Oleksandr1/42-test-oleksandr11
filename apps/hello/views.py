ee875e7acd5129ddac46cab52abe0b017dd6f588
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-03 16:45:17 +0200

    transfer data from db to view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import MyData
 # Create your views here.
 def main_page(request):
-    return render(request, 'hello/main.html', {})
+    my_data = MyData.objects.first()
+    return render(request, 'hello/main.html', {'my_data':my_data})
