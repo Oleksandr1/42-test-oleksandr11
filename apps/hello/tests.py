2602c8fcb4ce2237b33e8a7b38d4b1f74fd882eb
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-03 15:25:43 +0200

    corrected test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -2,8 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
-
 class SomeTests(TestCase):
     def test_math(self):
-        '''put docstrings in your tests'''
-        assert(2 + 2 == 4)
+        '''testing 2+2'''
+        self.assertEqual(2+2,4)
