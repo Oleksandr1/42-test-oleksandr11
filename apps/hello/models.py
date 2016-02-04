d24fb047e65ce65c2e4c733c28a5dd78ce0a48d9
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-04 14:44:21 +0200

    flake8

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -3,37 +3,38 @@ from django.db import models
 # Create your models here.
 class MyData(models.Model):
     first_name = models.CharField(
-    max_length=256,
-    blank=False,
-    verbose_name="Name")
+                                  max_length=256,
+                                  blank=False,
+                                  verbose_name="Name")
 
     last_name = models.CharField(
-    max_length=256,
-    blank=False,
-    verbose_name="Surname")
+                                 max_length=256,
+                                 blank=False,
+                                 verbose_name="Surname")
 
     birthday  = models.DateField(
-    blank=False,
-    verbose_name='Data of Birth',
-    null=True)
+                                 blank=False,
+                                 verbose_name='Data of Birth',
+                                 null=True)
 
-    email = models.CharField(max_length=256, blank=False,
-    verbose_name='E-mail')
+    email = models.CharField(
+                             max_length=256, blank=False,
+                             verbose_name='E-mail')
 
     skype = models.CharField(
-    max_length=256,
-    blank=False,
-    verbose_name='skype')
+                             max_length=256,
+                             blank=False,
+                             verbose_name='skype')
 
     jabber = models.CharField(
-    max_length=256,
-    blank=False,
-    verbose_name='Jabber/xmpp')
+                              max_length=256,
+                              blank=False,
+                              verbose_name='Jabber/xmpp')
 
     biography = models.TextField(
-    blank=True,
-    verbose_name='Biography')
+                                 blank=True,
+                                 verbose_name='Biography')
 
     other_contacts = models.TextField(
-    blank=True,
-    verbose_name='other contacts')
+                                      blank=True,
+                                      verbose_name='other contacts')
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
+
 class SomeTests(TestCase):
     def test_math(self):
         '''testing 2+2'''
