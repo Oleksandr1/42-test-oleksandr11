92ed1dfcd537351c5ed9502c0e9b417e8caf4080
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-05 14:24:43 +0200

    add empty line :)

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -7,5 +7,6 @@ def main_page(request):
     my_data = MyData.objects.first()
     return render(request, 'hello/main.html', {'my_data': my_data})
 
+
 def show_requests(request):
     return render(request, 'hello/requests.html', {})
