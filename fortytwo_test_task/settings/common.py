53fcdabea4aa97fd17278b3d2a38a103911c4d3c
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-05 17:36:11 +0200

    make middleware

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.HistoryMiddleware',
+    'fortytwo_test_task.middleware.HistoryMiddleware',
 )
 
 ROOT_URLCONF = 'fortytwo_test_task.urls'
