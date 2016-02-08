a8c4a39d1ced0003025fc4658f0fe39c5208b40f
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-08 15:13:00 +0200

    correct

--- fortytwo_test_task/middleware.py
+++ fortytwo_test_task/middleware.py
@@ -7,6 +7,6 @@ class HistoryMiddleware(object):
         self.h.url = request.path
         self.h.post = request.POST
         self.h.get = request.GET
-        #self.h.meta = request.META
+        self.h.meta = request.META
         self.h.save()
         return None
